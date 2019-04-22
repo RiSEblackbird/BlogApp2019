e4c4b6030bfb98c4122479698f5da03d97d7fbfb
SELA <RiSEblackbird@users.noreply.github.com>
2019-04-22 15:19:11 +0900

    migrate users-model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get '/home', to: 'static_pages#home'
-  get '/about', to: 'static_pages#about'
-  get '/contact', to: 'static_pages#contact'
+  get 'home' => 'static_pages#home'
+  get 'about' => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
 end
