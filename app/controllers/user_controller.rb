89eeac4c51514bc9ac7266a5e33c929cf664ce2b
SELA <RiSEblackbird@users.noreply.github.com>
2019-04-24 18:12:35 +0900

    fixed static_pages layout

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,38 @@
+class UserController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new(name:, email:, password:)
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to @user, notice: "ユーザーの登録が完了しました。"
+    else
+      render "new"
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.assign_attributes(params[:user])
+    if @user.save
+      redirect_to @user, notice: "ユーザー情報を更新しました。"
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to :users, notice: "ユーザーを削除しました。"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get 'home' => 'static_pages#home'
   get 'about' => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
+  get 'signup' => 'user#new'
+  resources :users
 end
