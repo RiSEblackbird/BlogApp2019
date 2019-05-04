92235ace0674458b51dbe7f979256c143e333cfc
SELA <RiSEblackbird@users.noreply.github.com>
2019-05-05 03:15:40 +0900

    prepare devise users

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'users/index'
+  get 'users/show'
   root 'static_pages#home'
   get 'home' => 'static_pages#home'
   get 'about' => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
-  get 'signup' => 'user#new'
-  resources :users
+  resources :users, :only => [:index, :show]
 end
