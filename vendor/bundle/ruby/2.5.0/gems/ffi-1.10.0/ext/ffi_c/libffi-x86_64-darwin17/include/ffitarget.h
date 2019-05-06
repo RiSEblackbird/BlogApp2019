a67d3bded1391113ba722532461d61c180b667ef
SELA <RiSEblackbird@users.noreply.github.com>
2019-05-06 19:04:55 +0900

    vender/bundleの削除と.bundle/config内のパス指定消去

--- vendor/bundle/ruby/2.5.0/gems/ffi-1.10.0/ext/ffi_c/libffi-x86_64-darwin17/include/ffitarget.h
+++
@@ -1,1 +0,0 @@
-/Users/Taishi/environment/PracApps/prac_app1/vendor/bundle/ruby/2.5.0/gems/ffi-1.10.0/ext/ffi_c/libffi/src/x86/ffitarget.h
--- vendor/bundle/ruby/2.5.0/gems/remotipart-1.4.2/lib/remotipart/rails/version.rb
+++
@@ -1,6 +0,0 @@
-module Remotipart
-  module Rails
-    VERSION = "1.4.2"
-    IFRAMETRANSPORT_VERSION = "02.06.2013"
-  end
-end
