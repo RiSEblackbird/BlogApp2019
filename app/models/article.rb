6737c6fe2f9358cca8b70df9be93c9cc96861437
SELA <RiSEblackbird@users.noreply.github.com>
2019-05-12 17:17:44 +0900

    バリデーションを追記

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,6 @@
 class Article < ApplicationRecord
+  validates :title, :body, :released_at, presence: true
+  validates :category, length: { maximum: 30 }
+  validates :title, length: { maximum: 100 }
+  validates :body, length: { maximum: 5000 }
 end
