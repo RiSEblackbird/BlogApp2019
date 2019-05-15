0f7edef6b345a998e48e74acc8c9b16ea2393f61
SELA <RiSEblackbird@users.noreply.github.com>
2019-05-15 18:20:26 +0900

    バリデーションの追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable,:trackable, :validatable
+  validates :username, presence: true,
+    format: { with: /\A[A-Za-z][A-Za-z0-9]*\z/, allow_blank: true, message: :invalid_user_username },
+    length: { minimum: 2, maximum: 20, allow_blank: true },
+    uniqueness: { case_sensitive: false }
 end
