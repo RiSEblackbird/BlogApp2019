af74cfae380203694d50f096d0dd804cdd6f5eb4
SELA <RiSEblackbird@users.noreply.github.com>
2019-05-16 13:58:20 +0900

    カラムに投稿者を追加

--- db/migrate/20190508125609_create_articles.rb
+++ db/migrate/20190508125609_create_articles.rb
@@ -2,6 +2,7 @@ class CreateArticles < ActiveRecord::Migration[5.2]
   def change
     create_table :articles do |t|
       t.string :category, null: false    #カテゴリ
+      t.string :username, null: false    #投稿者
       t.string :title, null: false    #タイトル
       t.text :boby, null: false    #本文
       t.datetime :released_at, null: false    #掲載開始日
