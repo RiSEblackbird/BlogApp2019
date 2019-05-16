dcb0e65d1adb91b2c1e9bdd4c521b134d82d096a
SELA <RiSEblackbird@users.noreply.github.com>
2019-05-16 13:58:46 +0900

    記事のルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   get 'about' => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   resources :users, :only => [:index, :show]
+  resources :articles
 end
