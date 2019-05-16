b8b719a30f7cea05e43b35c827fa6354c88460d9
SELA <RiSEblackbird@users.noreply.github.com>
2019-05-16 14:22:11 +0900

    :body 誤字修正

--- db/migrate/20190508125609_create_articles.rb
+++ db/migrate/20190508125609_create_articles.rb
@@ -4,7 +4,7 @@ class CreateArticles < ActiveRecord::Migration[5.2]
       t.string :category, null: false    #カテゴリ
       t.string :username, null: false    #投稿者
       t.string :title, null: false    #タイトル
-      t.text :boby, null: false    #本文
+      t.text :body, null: false    #本文
       t.datetime :released_at, null: false    #掲載開始日
 
       t.timestamps
