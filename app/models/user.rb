bb7f617926ddeec36225952ef2e60f42b0dd7988
SELA <mokut2017@gmail.com>
2019-05-25 19:27:08 +0900

    ユーザー名の半角英数制限を削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable,:trackable, :validatable
   validates :username, presence: true,
-    format: { with: /\A[A-Za-z][A-Za-z0-9]*\z/, allow_blank: true, message: :invalid_user_username },
     length: { minimum: 2, maximum: 20, allow_blank: true },
     uniqueness: { case_sensitive: false }
 end
