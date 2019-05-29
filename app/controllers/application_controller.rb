4b22f6aa9594d5b757ec4495890ac62132c78585
SELA <mokut2017@gmail.com>
2019-05-29 15:48:47 +0900

    :image カラム追加に伴う編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,9 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
+  # ストロングパラメータを定義
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :image])
       devise_parameter_sanitizer.permit(:sign_in, keys: [:username])
       devise_parameter_sanitizer.permit(:account_update, keys: [:username])
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,21 @@
 class User < ApplicationRecord
+
+  # Articleモデルとの関連付け
   has_many :articles, dependent: :destroy
+
+  # プロフ写真との関連付け
+  mount_uploader :image, ImageUploader
+
+  # Userモデルに適用するdeviseの機能
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable,:trackable, :validatable
+
+  # Userモデルのカラムのバリデーション
   validates :username, presence: true,
     length: { minimum: 2, maximum: 20, allow_blank: true },
     uniqueness: { case_sensitive: false }
+
+  def resized_image
+    return self.image.variant(resize: '100x100')
+  end
 end
