d1aa31dfa7fb053e3d95e93c9acaf52fe5f63739
SELA <mokut2017@gmail.com>
2019-06-01 11:23:31 +0900

    補足用コメント文追記

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
 
+  # deviseのアクションにストロングパラメータを適用する
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -7,6 +8,6 @@ class ApplicationController < ActionController::Base
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :image])
       devise_parameter_sanitizer.permit(:sign_in, keys: [:username])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:username, :profile])
     end
 end
