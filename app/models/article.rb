06befa29c3d6e1e1960c882ba3a42f105b0241a4
SELA <mokut2017@gmail.com>
2019-06-08 21:03:34 +0900

    コメント機能のサーバーサイド実装

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 class Article < ApplicationRecord
   acts_as_taggable
   belongs_to :user
+  has_many :comments, dependent: :destroy
+
   validates :title, :body, presence: true
   validates :tag_list, length: { maximum: 20 }
   validates :title, length: { maximum: 100 }
