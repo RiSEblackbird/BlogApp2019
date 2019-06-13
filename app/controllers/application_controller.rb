3fce9ace2d5eb5131f78809bf685205913f3ca39
SELA <mokut2017@gmail.com>
2019-06-13 22:42:49 +0900

    ストロングパラにパスワード追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,6 @@ class ApplicationController < ActionController::Base
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :image])
       devise_parameter_sanitizer.permit(:sign_in, keys: [:username])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:username, :profile])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:username, :profile, :password])
     end
 end
