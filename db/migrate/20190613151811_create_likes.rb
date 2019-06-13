a2413ec50d8d7be83c9caee18f76d65fdb6e1c92
SELA <mokut2017@gmail.com>
2019-06-14 00:19:57 +0900

    Likeモデル生成

---
+++ db/migrate/20190613151811_create_likes.rb
@@ -0,0 +1,11 @@
+class CreateLikes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :likes do |t|
+      t.integer :user_id
+      t.integer :article_id
+      t.integer :comment_id
+
+      t.timestamps
+    end
+  end
+end
