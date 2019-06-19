014160548840a70af74eef98034d43b3e2187274
SELA <mokut2017@gmail.com>
2019-06-19 14:48:04 +0900

    authorメソッド定義

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,12 @@
 class Article < ApplicationRecord
   acts_as_taggable
   belongs_to :user
+  belongs_to :author, class_name: "User", foreign_key: "user_id"
   has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  def like_user(user_id)
+    Likes.find_by(user_id: user_id)
+  end
 
   validates :title, :body, presence: true
   validates :tag_list, length: { maximum: 20 }
