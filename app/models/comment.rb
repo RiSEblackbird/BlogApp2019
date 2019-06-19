99e3ebe789d9f19d1473f8cc98a70f5a5ebae161
SELA <mokut2017@gmail.com>
2019-06-19 14:48:31 +0900

    commenterメソッド定義

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,11 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :article
+  belongs_to :commenter, class_name: "User", foreign_key: "user_id"
+  has_many :likes, dependent: :destroy
+  def like_user(user_id)
+    likes.find_by(user_id: user_id)
+  end
+
   validates :content, length: { maximum: 500 }, presence: true
 end
