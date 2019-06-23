effa8819c2aa49be0650a236f25b90511780eaa0
SELA <mokut2017@gmail.com>
2019-06-24 03:43:32 +0900

    コメント一覧のページネーション描写(注意：#40)

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,7 +9,7 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
     @comment = @article.comments.new
-    @comments = @article.comments.page(params[:page]).per(5).order(created_at: :desc)
+    @comments = @article.comments.order(created_at: :desc)
     @like = @article.likes.new
     @likes = @article.likes.where(article_id: params[:article_id])
   end
