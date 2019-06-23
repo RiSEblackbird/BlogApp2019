07a2657f9c6ad7313863ac7a86279f42cc93ba2d
SELA <mokut2017@gmail.com>
2019-06-23 12:17:02 +0900

    Like関連、記事数カウント

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,11 +1,11 @@
 class Article < ApplicationRecord
   acts_as_taggable
-  belongs_to :user
+  belongs_to :user, :counter_cache => :article_count
   belongs_to :author, class_name: "User", foreign_key: "user_id"
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   def like_user(user_id)
-    Likes.find_by(user_id: user_id)
+    likes.find_by(user_id: user_id)
   end
 
   validates :title, :body, presence: true
