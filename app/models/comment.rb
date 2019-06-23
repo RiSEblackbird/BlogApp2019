32b252e7844b26c96d7e2c506ca4e8f8370a28d0
SELA <mokut2017@gmail.com>
2019-06-23 12:17:16 +0900

    コメント数カウント

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
-  belongs_to :user
-  belongs_to :article
+  belongs_to :user, :counter_cache => :comment_count
+  belongs_to :article, :counter_cache => :comment_count
   belongs_to :commenter, class_name: "User", foreign_key: "user_id"
   has_many :likes, dependent: :destroy
   def like_user(user_id)
