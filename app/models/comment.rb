8bbfccf80e48614891f8466fd8959c5bacd7c2e7
SELA <mokut2017@gmail.com>
2019-06-25 21:39:40 +0900

    空行変更

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,9 +3,10 @@ class Comment < ApplicationRecord
   belongs_to :article, :counter_cache => :comment_count
   belongs_to :commenter, class_name: "User", foreign_key: "user_id"
   has_many :likes, dependent: :destroy
+
+  validates :content, length: { maximum: 500 }, presence: true
+
   def like_user(user_id)
     likes.find_by(user_id: user_id)
   end
-
-  validates :content, length: { maximum: 500 }, presence: true
 end
