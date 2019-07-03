14f664eb4bd5f1f0f16172c1984aee5d42dfa4c3
SELA <mokut2017@gmail.com>
2019-07-04 02:02:37 +0900

    フォロー/解除のAjax処理化

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,14 +2,15 @@ class RelationshipsController < ApplicationController
   def create
     @user = User.find(params[:relationship][:following_id])
     @relationship = current_user.follow!(@user)
-    @relationship.save
-    if @relationship.save
-      flash[:notice] = @user.username += "さんをフォローしました！"
-      redirect_back(fallback_location: root_path)
-    else
-      flash[:notice] = @user.username += "さんのフォローに失敗しました。"
-      redirect_back(fallback_location: root_path)
-    end
+    @user.reload
+    #@relationship.save
+    #if @relationship.save
+    #  flash[:notice] = @user.username += "さんをフォローしました！"
+    #  redirect_back(fallback_location: root_path)
+    #else
+    #  flash[:notice] = @user.username += "さんのフォローに失敗しました。"
+    #  redirect_back(fallback_location: root_path)
+    #end
   end
 
   def destroy
@@ -17,12 +18,13 @@ class RelationshipsController < ApplicationController
     followed = Relationship.find(params[:id]).following
     @relationship = current_user.unfollow!(@user)
     @relationship.destroy
-    if @relationship.destroy
-      flash[:notice] = followed.username += "さんのフォローを解除しました。"
-      redirect_back(fallback_location: root_path)
-    else
-      flash[:notice] = followed.username += "さんのフォローの解除に失敗しました。"
-      redirect_back(fallback_location: root_path)
-    end
+    @user.reload
+    #if @relationship.destroy
+    #  flash[:notice] = followed.username += "さんのフォローを解除しました。"
+    #  redirect_back(fallback_location: root_path)
+    #else
+    #  flash[:notice] = followed.username += "さんのフォローの解除に失敗しました。"
+    #  redirect_back(fallback_location: root_path)
+    #end
   end
 end
