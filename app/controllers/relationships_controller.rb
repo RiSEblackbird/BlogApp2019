e19f39926025002b0f58d8cfedb64b94b1f23a75
SELA <mokut2017@gmail.com>
2019-07-09 16:44:45 +0900

    [ Fix ]. @user.reload ・解除ボタン実行時のエラーは解消された。 ・遷移後フォローボタンが表示されず消えた状態になる。

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -8,14 +8,14 @@ class RelationshipsController < ApplicationController
 
   def destroy
     @user = Relationship.find(params[:id]).follower
-    #p "@user >>> ", @user
+    p "@user >>> ", @user
     followed = Relationship.find(params[:id]).following
-    #p "followed >>> ", followed
-    #p "current_user.following_relationships.find_by(follower_id: current_user.id)", current_user.following_relationships.find_by(follower_id: current_user.id)
+    p "followed >>> ", followed
+    p "current_user.following_relationships.find_by(follower_id: current_user.id)", current_user.following_relationships.find_by(follower_id: current_user.id)
     @relationship = current_user.unfollow!(current_user)
-    #p "@relationship >>> ", @relationship
+    p "@relationship >>> ", @relationship
     @relationship.destroy
-    @user = User.find(params[:id])
-    #p "@user >>> ", @user
+    @user.reload
+    p "@user >>> ", @user
   end
 end
