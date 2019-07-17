cb28f6d725f760275654e0bf90658a24b422c3aa
SELA <mokut2017@gmail.com>
2019-07-17 18:32:12 +0900

    FIX. vue読み込み試し ・読み込めているものの文字がうまく表示されていない。

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -9,9 +9,6 @@ import Vue from 'vue'
 import App from '../app.vue'
 
 document.addEventListener('DOMContentLoaded', () => {
-  const app = new Vue({
-    render: h => h(App)
-  }).$mount()
   document.body.appendChild(document.createElement('hello'))
   const app = new Vue(App).$mount('hello')
 
