d90dd6cf43489cd11957630ade0e378851b82285
SELA <mokut2017@gmail.com>
2019-08-31 16:14:15 +0900

    ADD. Action(edit) to tags_controller ・Think about 'destroy' on the other time.

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,5 +1,5 @@
 class TagsController < ApplicationController
-  
+
   def index
     @tags = ActsAsTaggableOn::Tag.all
   end
@@ -7,4 +7,24 @@ class TagsController < ApplicationController
   def show
     @tag = ActsAsTaggableOn::Tag.find(params[:id])
   end
+
+  def edit
+    @tag = ActsAsTaggableOn::Tag.find(params[:id])
+  end
+
+  def update
+    @tag = ActsAsTaggableOn::Tag.find(params[:id])
+    @tag.assign_attributes(tag_params)
+    if @tag.save
+      redirect_to @tag, notice: "タグの情報を更新しました！"
+    else
+      render "edit"
+    end
+  end
+
+  private
+
+   def tag_params
+     params.require(:tag).permit(:description)
+   end
 end
