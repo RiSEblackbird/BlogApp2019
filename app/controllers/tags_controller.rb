ec718bc498f9d1efda924ebe6d179a0a650203fe
SELA <mokut2017@gmail.com>
2019-08-31 16:02:54 +0900

    ADD. Define actions(index, show) to tags_controller.rb

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,2 +1,10 @@
 class TagsController < ApplicationController
+  
+  def index
+    @tags = ActsAsTaggableOn::Tag.all
+  end
+
+  def show
+    @tag = ActsAsTaggableOn::Tag.find(params[:id])
+  end
 end
