f939f3919c4fb67d2d3c28f9ffc8e87503717621
SELA <mokut2017@gmail.com>
2019-09-24 23:02:00 +0900

    REM. dir '/test'

--- test/factories/comments.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :comment do
-    content { "MyString" }
-    article { "" }
-  end
-end
--- test/factories/likes.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :like do
-    user_id { 1 }
-    article_id { 1 }
-    comment_id { 1 }
-  end
-end
