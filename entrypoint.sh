65b175549fd7dafbab95c2e0cf3c3fd800231636
TAISHI SENOH <taishi.senoo@jdsc.ai>
2021-02-23 16:44:31 +0900

    Create entrypoint.sh
    (追加を忘れてた)

---
+++ entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+# Rails に対応したファイル server.pid が存在しているかもしれないので削除する。
+rm -f /myapp/tmp/pids/server.pid
+
+# コンテナーのプロセスを実行する。（Dockerfile 内の CMD に設定されているもの。）
+exec "$@"
+
+# This script from "https://matsuand.github.io/docs.docker.jp.onthefly/compose/rails/#define-the-project"
